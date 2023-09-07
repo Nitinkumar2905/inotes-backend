26b247c2b4bc0b19d7d786896b2331839603a892
nitin <nimble2905@gmail.com>
2023-09-07 11:31:34 +0530

    some changes

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ connectToMongo();
 const app = express();
 const port = 5000;
 app.use(express.json());
-app.use(cors(
+app.use(
+  cors(
     {
-    origin: ["https://inotes-f.vercel.app/login"],
+    origin: ["https://inotes-f.vercel.app/"],
     methods: ["POST", "DELETE", "GET","PUT"],
     credentials: true,
   }
@@ -23,5 +24,5 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-  console.log(`Inotes running at http://localhost:${port}`);
+  console.log(`Inotes backend running at http://localhost:${port}`);
 });
